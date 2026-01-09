823074b9339eb7dcc92151b4b0bcd1dc49325afd
Etienne Cossalter <e.cossalter@cyberg.fr>
2026-01-09 12:47:09 +0100

    add save db http

--- src/web/express.js
+++ src/web/express.js
@@ -11,6 +11,14 @@ app.get('/leaderboard', async (req, res) => {
     res.send(html);
 });
 
+app.get("/export/db", (req, res) => {
+  if (req.query.key !== process.env.EXPORT_KEY) {
+    return res.sendStatus(403);
+  }
+
+  res.download(process.env.DB_PATH, `scores-${Date.now()}.db`);
+});
+
 // Démarre le serveur
 app.listen(PORT, () => console.log(`✅ Leaderboard accessible sur http://localhost:${PORT}/leaderboard`));
 
